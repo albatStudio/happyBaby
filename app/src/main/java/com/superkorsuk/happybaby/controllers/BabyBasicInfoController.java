94cc8ac396833db3d20a5cee30c66ee5591b52c3
superbong <bong.heo@gmail.com>
2016-09-25 13:42:43 +0900

    baby basic info

---
+++ app/src/main/java/com/superkorsuk/happybaby/controllers/BabyBasicInfoController.java
@@ -0,0 +1,40 @@
+package com.superkorsuk.happybaby.controllers;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.superkorsuk.happybaby.R;
+import com.superkorsuk.happybaby.db.BabyRepository;
+import com.superkorsuk.happybaby.models.Baby;
+
+import java.util.List;
+
+public class BabyBasicInfoController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_baby_basic_info_controller);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private void displayBabyInfo() {
+        BabyRepository baby;
+        List<Baby> babyList = baby.all()
+    }
+
+}
