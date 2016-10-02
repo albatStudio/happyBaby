1e71f2f0fcdb696562d3c193d852df6fe39705a4
superbong <bong.heo@gmail.com>
2016-10-02 20:10:17 +0900

    특정일 통계에서 날짜 선택시 각 수유, 응가, 잠에 대한 listview 보여주는 로직 추가(개념만)

--- app/src/main/java/com/superkorsuk/happybaby/views/StatisticsCalendarActivity.java
+++ app/src/main/java/com/superkorsuk/happybaby/views/StatisticsCalendarActivity.java
@@ -4,21 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.superkorsuk.happybaby.R;
 import com.superkorsuk.happybaby.adapters.ExpandableListViewAdapter;
 import com.superkorsuk.happybaby.db.BabyDoRepository;
 import com.superkorsuk.happybaby.db.BabyRepository;
+import com.superkorsuk.happybaby.models.BabyDo;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import me.nlmartian.silkcal.DatePickerController;
 import me.nlmartian.silkcal.DayPickerView;
 import me.nlmartian.silkcal.SimpleMonthAdapter;
 
+import static junit.framework.Assert.assertTrue;
+
 public class StatisticsCalendarActivity extends AppCompatActivity implements DatePickerController {
+
+    private static final int BABY_ID = 1;
 
     private DayPickerView calendarView;
     private TextView selectedDayTextView;
@@ -62,22 +69,20 @@ public class StatisticsCalendarActivity extends AppCompatActivity implements Dat
         recyclerview = (RecyclerView) findViewById(R.id.LV_statistics_selected_day);
         recyclerview.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
 
+
+/*        // 날짜 선택시 list view에 데이타를 넣어주는 example
         List<ExpandableListViewAdapter.Item> selectedDayData = new ArrayList<>();
 
         // for breastfeeding (수유)
-        ExpandableListViewAdapter.Item breastfeeding = new ExpandableListViewAdapter.Item(ExpandableListViewAdapter.HEADER, "수유");
-        breastfeeding.invisibleChildren = new ArrayList<>();
-        breastfeeding.invisibleChildren.add(new ExpandableListViewAdapter.Item(ExpandableListViewAdapter.CHILD, "10시 05분: 10ml"));
-        breastfeeding.invisibleChildren.add(new ExpandableListViewAdapter.Item(ExpandableListViewAdapter.CHILD, "16시 30분: 10ml"));
-
-        selectedDayData.add(breastfeeding);
+        selectedDayData.add(new ExpandableListViewAdapter.Item(ExpandableListViewAdapter.HEADER, "수유"));
+        selectedDayData.add(new ExpandableListViewAdapter.Item(ExpandableListViewAdapter.CHILD, "10시 05분: 10ml"));
+        selectedDayData.add(new ExpandableListViewAdapter.Item(ExpandableListViewAdapter.CHILD, "16시 30분: 10ml"));
 
         // for poop (대변)
         ExpandableListViewAdapter.Item poop = new ExpandableListViewAdapter.Item(ExpandableListViewAdapter.HEADER, "응가");
         poop.invisibleChildren = new ArrayList<>();
         poop.invisibleChildren.add(new ExpandableListViewAdapter.Item(ExpandableListViewAdapter.CHILD, "07시 13분: 일반똥"));
         poop.invisibleChildren.add(new ExpandableListViewAdapter.Item(ExpandableListViewAdapter.CHILD, "13시 45분: 설사"));
-
         selectedDayData.add(poop);
 
         // for duration (수면)
@@ -85,10 +90,11 @@ public class StatisticsCalendarActivity extends AppCompatActivity implements Dat
         duration.invisibleChildren = new ArrayList<>();
         duration.invisibleChildren.add(new ExpandableListViewAdapter.Item(ExpandableListViewAdapter.CHILD, "22:10 ~ 07:43"));
         duration.invisibleChildren.add(new ExpandableListViewAdapter.Item(ExpandableListViewAdapter.CHILD, "09:32 ~ 11:12"));
-
         selectedDayData.add(duration);
 
-        recyclerview.setAdapter(new ExpandableListViewAdapter(selectedDayData));
+        recyclerview.setAdapter(new ExpandableListViewAdapter(selectedDayData));*/
+
+        babyDoShow(year, month, day);
     }
 
     @Override
@@ -96,7 +102,31 @@ public class StatisticsCalendarActivity extends AppCompatActivity implements Dat
 
     }
 
-    public void babyDoFormulaShow(int year, int month, int day) {
+    public void babyDoShow(int year, int month, int day) {
+        List<ExpandableListViewAdapter.Item> selectedDayData = new ArrayList<>();
+        BabyDoRepository repo = new BabyDoRepository(getApplicationContext());
+
+        //TODO :: FORMULA, BREAST_MILK, POOP, SLEEP 4가지 항목에 대해 특정일 리스트가 필요
+//        List<BabyDoFormula> babyDoListFormula = repo.getAllFormulaAt(BABY_ID, year, month, day);
+//        List<BabyDoMilk> babyDoListMilk = repo.getAllMilkAt(BABY_ID, year, month, day);
+//        List<BabyDoPoop> babyDoListPoop = repo.getAllPoopAt(BABY_ID, year, month, day);
+//        List<BabyDoSleep> babyDoListSleep = repo.getAllSleepAt(BABY_ID, year, month, day);
+
+/*        selectedDayData.add(new ExpandableListViewAdapter.Item(ExpandableListViewAdapter.HEADER, "수유"));
+        for(BabyDoFormula formula : babyDoListFormula) {
+            int amount, breastfeedingLeft, breastfeedingRight;
+            Date issueDate;
+            String note;
+
+            issueDate = formula.getIssueDate();
+            amount = formula.getAmount();
+            breastfeedingLeft = formula.getBreastfeedingLeft();
+            breastfeedingRight = formula.getBreastfeedingRight();
+
+            String data = "" + issueDate + "";
+            selectedDayData.add(new ExpandableListViewAdapter.Item(ExpandableListViewAdapter.CHILD, data));
+        }*/
+
     }
 
     public void babyDoMilkShow(int year, int month, int day) {
